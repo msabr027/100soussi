625721290cab1d8fa032191cfebdd6ef7a8b218d
Mohamed Sabri <mohamed@datalyticsbusiness.com>
2019-11-03 11:40:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from datetime import date
 import schedule
 from sqlalchemy import create_engine
 
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
